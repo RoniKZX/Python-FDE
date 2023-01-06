43baa09b5b19ad955476d80a242bcb5f6d4492af
kal <101020782+RoniKZX@users.noreply.github.com>
2023-01-06 13:51:09 -0400

    Improve code for solution

--- Control-Flow/Scripting/Practice2/match_flower_name.py
+++ Control-Flow/Scripting/Practice2/match_flower_name.py
@@ -17,30 +17,22 @@ Sample Output:
 """
 
 # Write your code here
-def getUserName() -> str:
-    userName = input("Enter your First [space] Last name only: ")
-    return userName.split(' ')
-
 def createFlowerDict(filename='flowers.txt') -> dict:
     flowersDict = {}
 
     with open(filename, 'r') as flowers:
         for flower in flowers:
             flower = flower.split(': ')
-            flowersDict[flower[0]] = flower[1].strip().title()
+            flowersDict[flower[0].capitalize()] = flower[1].strip().title()
 
     return flowersDict
 
-def getUniqueFlower(flowers: dict, userName: str) -> str:
-    uniqueFlower = ''
-    for letter, flower in flowers.items():
-        if userName[0][0] == letter:
-            uniqueFlower = f'Unique flower name with the first letter ({letter}): {flower}'
-            break
+def main() -> None:
+    flowerDict = createFlowerDict()
+    fullName = input("Enter your First [space] Last name only: ")
+    firstName = fullName.split(' ')[0][0].capitalize()
 
-    print(uniqueFlower)
+    print(f'Unique flower name with the first letter ({firstName[0].capitalize()}): {flowerDict[firstName[0]]}')
 
 # print the desired output
-userName = getUserName()
-flowers = createFlowerDict()
-getUniqueFlower(flowers, userName)
+main()
