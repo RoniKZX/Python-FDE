f931cf4ac8973f78876732332eb6166635f8f2d4
kal <101020782+RoniKZX@users.noreply.github.com>
2023-01-13 12:23:26 -0400

    Add Product class

--- Python-OOP/E2-bank.py
+++ Python-OOP/E2-bank.py
@@ -84,28 +84,55 @@ class Fraction:
 
         return Fraction((self.nr*x.dr + x.nr*self.dr), (self.dr*x.dr))
 
+class Product():
+    def __init__(self, id, marked_price, discount):
+        self.id = id
+        self.marked_price = marked_price
+        self._discount = discount
+    
+    def display(self):
+        print(self.id,  self.marked_price,  self.discount)
+    
+    @property
+    def selling_price(self):
+        return self.marked_price - self.marked_price * self.discount * 0.01
+
+    @property
+    def discount(self):
+        return self._discount+2 if self.marked_price > 500 else self._discount 
+
+    @discount.setter
+    def discount(self, new_discount):
+        self._discount = new_discount
+
 if __name__ == '__main__':
-    book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,10)
-    book2 = Book('652-6-86-748413-3', 'Learn Chemistry','Jack', 'CBC', 400, 220,20)
-    book3 = Book('957-7-39-347216-2', 'Learn Maths','John', 'XYZ', 500, 300,5)
-    book4 = Book('957-7-39-347216-2', 'Learn Biology','Jack', 'XYZ', 400, 200,6)
+    # book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,10)
+    # book2 = Book('652-6-86-748413-3', 'Learn Chemistry','Jack', 'CBC', 400, 220,20)
+    # book3 = Book('957-7-39-347216-2', 'Learn Maths','John', 'XYZ', 500, 300,5)
+    # book4 = Book('957-7-39-347216-2', 'Learn Biology','Jack', 'XYZ', 400, 200,6)
 
-    books = [book1, book2, book3, book4]
+    # books = [book1, book2, book3, book4]
 
-    for book in books:
-        book.display()
+    # for book in books:
+    #     book.display()
 
-    print([j.title for j in books if j.author == 'Jack'])
+    # print([j.title for j in books if j.author == 'Jack'])
 
-    f1 = Fraction(2,3)
-    f1.show()
-    f2 = Fraction(3,4)
-    f2.show()
-    f3 = f1.multiply(f2)
-    f3.show()
-    f3 = f1.add(f2)
-    f3.show()
-    f3 = f1.add(5) 
-    f3.show()
-    f3 = f1.multiply(5) 
-    f3.show()
+    # f1 = Fraction(2,3)
+    # f1.show()
+    # f2 = Fraction(3,4)
+    # f2.show()
+    # f3 = f1.multiply(f2)
+    # f3.show()
+    # f3 = f1.add(f2)
+    # f3.show()
+    # f3 = f1.add(5) 
+    # f3.show()
+    # f3 = f1.multiply(5) 
+    # f3.show()
+
+    p1 = Product('X879', 400, 6)
+    p2 = Product('A234', 100, 5)
+    p3 = Product('B987', 990, 4)
+    p4 = Product('H456', 800, 6)
+    
